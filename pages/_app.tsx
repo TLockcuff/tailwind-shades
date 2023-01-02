d99163e03f31e5004d5ac91820327837622c9790
Travis Lockcuff <tlockcuff@gmail.com>
2023-01-02 12:19:38 -0500

    Adding Plausible

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest"></link>
+        <script defer data-domain="tailwindshades.app" src="https://insights.tlockcuff.dev/js/script.js"></script>
       </Head>
       <Component {...pageProps} />
     </>
